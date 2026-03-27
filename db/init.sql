8c8a61646b0d9ab06edc04114f5cf04875b4a79e
hiba <touikhiba@gmail.com>
2026-03-27 10:23:04 +0100

    corriger

--- db/init.sql
+++ db/init.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS flaskdb;
 
 USE flaskdb;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     email text
 );
-INSERT INTO users (name) VALUES ('salim'),('Nour'),('Youssef');
+INSERT INTO users(name) VALUES ('Salim'),('Nour'),('Yousef');
